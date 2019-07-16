1a52e1d04edee8e153b8db5060be649803c7264f
Lauriel Mesquita <laurielmesquita@me.com>
2019-07-16 20:24:34 -0300

    Sequelize added and configured

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,9 @@ import { Router } from 'express'
 // We'll work only the routes here
 const routes = new Router()
 
+// Methods of routes
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' })
+  return res.json({ message: 'Hello Lauriel' })
 })
 
 export default routes
