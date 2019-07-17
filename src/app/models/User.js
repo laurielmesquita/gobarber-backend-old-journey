e398385ba25f430f2241aa5d92613aecaabbc448
Lauriel Mesquita <laurielmesquita@me.com>
2019-07-17 03:24:57 -0300

    User model is ready

---
+++ src/app/models/User.js
@@ -0,0 +1,27 @@
+import Sequelize, { Model } from 'sequelize'
+
+class User extends Model {
+  // Método stático que é chamado automáticamente pelo sequelize
+  static init (sequelize) {
+    // super.init() Chamando o método init da classe pai
+    // super é passado como primeiro parâmetro,
+    // é um objeto contendo todas as colunas
+    super.init(
+      {
+        // Vamos enviar as colunas da nossa base de dados
+        // Somente as colunas que serão inseridas pelo user
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_has: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN
+      },
+      // Aqui é o segundo parâmetro com o objeto sequelize
+      // Aqui também poderia passar mais configurações
+      {
+        sequelize
+      }
+    )
+  }
+}
+
+export default User
