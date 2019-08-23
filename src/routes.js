02b3e115ff767bfb0eaf6363703afd7c59764be9
Lauriel Mesquita <laurielmesquita@me.com>
2019-08-23 02:11:26 -0300

    Listing provider notifications Ok

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController'
 import ProviderController from './app/controllers/ProviderController'
 import AppointmentController from './app/controllers/AppointmentController'
 import ScheduleController from './app/controllers/ScheduleController'
+import NotificationController from './app/controllers/NotificationController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -20,7 +21,7 @@ routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 
 // Como o middlaware global está sendo colocado nesse ponto
-// todos as rotas a partir daqui já contarão com esse middleware,
+// todas as rotas a partir daqui já contarão com esse middleware
 // as rotas anteriores não passarão por esse middleware
 routes.use(authMiddleware)
 
@@ -32,6 +33,8 @@ routes.get('/appointments', AppointmentController.index)
 routes.post('/appointments', AppointmentController.store)
 
 routes.get('/schedule', ScheduleController.index)
+
+routes.get('/notifications', NotificationController.index)
 
 // Rota files com upload de apenas um arquivo por vez
 // O nome do campo que vamos enviar dentro da requisição file
