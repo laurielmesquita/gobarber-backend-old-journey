06bc8c5488a092d6f34dccd3e7cdd676bede837a
Lauriel Mesquita <laurielmesquita@me.com>
2019-08-26 20:17:50 -0300

    Configuring Nodemailer: Ok

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3222434540fe5a',
+    pass: '9223c59e23daf8'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+}
